78e872c4fae3f90edb4759cbdf1f7fb5bbb8f8b3
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2023-07-09 12:46:53 -0500

    Create Legna.h

---
+++ Legna.h
@@ -0,0 +1,30 @@
+#pragma once
+int SumOfTwoNumbers(int a, int b)
+{
+	return (a + b);
+}
+
+class Pikmin {
+public:
+	int RedPikmin = 0;
+	int BluePikmin = 0;
+	int YellowPikmin = 0;
+};
+
+class Pikmin2 {
+public:
+	int RedPikmin = 0;
+	int BluePikmin = 0;
+	int YellowPikmin = 0;
+	int PurplePikmin = 0;
+	int WhitePikmin = 0;
+};
+
+class Pikmin3 {
+public:
+	int RedPikmin = 0;
+	int BluePikmin = 0;
+	int YellowPikmin = 0;
+	int RockPikmin = 0;
+	int WingedPikmin = 0;
+};
